cb9bf4bf93e6e716a21e93e74b7dc86c95ec9f72
Eric Bunese <ericbunese@gmail.com>
2018-04-18 19:51:09 -0300

    Initial Commit

---
+++ vingeneric/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vingeneric
+//
+//  Created by Eric Bunese on 18/04/2018.
+//  Copyright © 2018 Eric Bunese. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
