1c1a6d52ae7160d548bc3a305c6fa5f668524c12
Eric Bunese <ericbunese@gmail.com>
2018-04-18 23:25:10 -0300

    Added several constant digits and some emoji functionality

--- vingeneric/vingenere.swift
+++ vingeneric/vingenere.swift
@@ -13,7 +13,7 @@ class Vingenere {
   var key: String = ""
   var cypher: String = ""
   
-  private let const = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+  private let const = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, 3, 9, 9, 3, 7, 5, 1, 0, 5, 8, 2, 0, 9, 7, 4, 9, 4, 4, 5, 9, 2, 3, 0, 7, 8, 1, 6, 4, 0, 6, 2, 8, 6, 2, 0, 8, 9, 9, 8, 6, 2, 8, 0, 3, 4, 8, 2, 5, 3, 4, 2, 1, 1, 7, 0, 6, 7, 9, 8, 2, 1, 4, 8, 0, 8, 6, 5, 1, 3, 2, 8, 2, 3, 0, 6, 6, 4, 7, 0, 9, 3, 8, 4, 4, 6, 0, 9, 5, 5, 0, 5, 8, 2, 2, 3, 1, 7, 2, 5, 3, 5, 9, 4, 0, 8, 1, 2, 8, 4, 8, 1, 1, 1, 7, 4, 5, 0, 2, 8, 4, 1, 0, 2, 7, 0, 1, 9, 3, 8, 5, 2, 1, 1, 0, 5, 5, 5, 9, 6, 4, 4, 6, 2, 2, 9, 4, 8, 9, 5, 4, 9, 3, 0, 3, 8, 1, 9, 6, 4, 4, 2, 8, 8, 1, 0, 9, 7, 5, 6, 6, 5, 9, 3, 3, 4, 4, 6, 1, 2, 8, 4, 7, 5, 6, 4, 8, 2, 3, 3, 7, 8, 6, 7, 8, 3, 1, 6, 5, 2, 7, 1, 2, 0, 1, 9, 0, 9, 1]
   private var alphabet: [String: [String]] = [:]
   
   init(text: String, key: String){
@@ -29,10 +29,12 @@ class Vingenere {
     
     for c in self.text {
       let k = self.key[self.key.index(self.key.startIndex, offsetBy: (pos % self.key.count))]
-      let alf = self.alphabet["\(k)"]
-      let off = self.const[(prod+pos) % self.key.count]
-      let chr = alf?.index(of: "\(c)")!
-      str += igl![(chr!+off) % (igl?.count)!]
+      if let alf = self.alphabet["\(k)"]{
+        let off = self.const[(prod+pos) % self.key.count]
+        if let chr = alf.index(of: "\(c)"){
+          str += igl![(chr+off) % (igl?.count)!]
+        }
+      }
       pos = pos+1
     }
     
@@ -47,10 +49,11 @@ class Vingenere {
     
     for c in self.cypher{
       let k = self.key[self.key.index(self.key.startIndex, offsetBy: (pos % self.key.count))]
-      let alf = self.alphabet["\(k)"]
-      let off = self.const[(prod+pos) % self.key.count]
-      let chr = igl?.index(of: "\(c)")
-      str += alf![self.modulo(val: (chr!-off), low: 0, upp: (alf?.count)!)]
+      if let alf = self.alphabet["\(k)"]{
+        let off = self.const[(prod+pos) % self.key.count]
+        let chr = igl?.index(of: "\(c)")
+        str += alf[self.modulo(val: (chr!-off), low: 0, upp: alf.count)]
+      }
       pos = pos+1
     }
     
@@ -69,7 +72,7 @@ class Vingenere {
   }
   
   private func initAlphabet(){
-    var alf = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Ã", "Â", "Á", "À", "Ê", "É", "È", "Î", "Í", "Ì", "Õ", "Ô", "Ó", "Ò", "Û", "Ú", "Ù", ".", ",", ":", ";", "!", "?", "@", "#", "$", "%", "&", "*", "(", ")", "-", "+", "=", "_", "/", "\\", "\"", "´", "`", "ˆ", "~", "[", "{", "]", "}", "<", ">"]
+    var alf = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Ã", "Â", "Á", "À", "Ê", "É", "È", "Î", "Í", "Ì", "Õ", "Ô", "Ó", "Ò", "Û", "Ú", "Ù", ".", ",", ":", ";", "!", "?", "@", "#", "$", "%", "&", "*", "(", ")", "-", "+", "=", "_", "/", "\\", "\"", "´", "`", "ˆ", "~", "[", "{", "]", "}", "<", ">", "ã", "â", "á", "à", "ê", "é", "è", "î", "í", "ì", "õ", "ô", "ó", "ò", "û", "ú", "ù", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ç", "Ç"]
     let emojiRanges = [
       0x1F601...0x1F64F,
       0x2702...0x27B0,
@@ -79,7 +82,7 @@ class Vingenere {
 
     for range in emojiRanges {
         for i in range {
-          var c = String(UnicodeScalar(i)!)
+          let c = String(UnicodeScalar(i)!)
             alf.append(c)
         }
     }
